4e8e00253336d1cf1a7c5783177137539d3d74c7
Viacheslav Matrizaev <matrizaev@gmail.com>
2024-05-09 22:35:24 +0300

    added actix-web

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,19 @@ use std::ffi::CString;
 
 use librtpkcs11::{perform_signing, TMemoryPointer};
 
-fn main() {
+use actix_web::{get, middleware::Logger, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+fn sign() {
     let input = CString::new("Hello World").expect("can't create a cstring");
     let user_pin = CString::new("12345678").expect("can't create a cstring");
     let key_pair_id = CString::new("12345678").expect("can't create a cstring");
@@ -17,9 +29,23 @@ fn main() {
             perform_signing(memory_pointer, user_pin.into_raw(), key_pair_id.into_raw());
         if memory_pointer.length > 0 && !memory_pointer.data.is_null() {
             println!("{}", memory_pointer.length);
-            // libc::free(memory_pointer.data as *mut libc::c_void);
+            libc::free(memory_pointer.data as *mut libc::c_void);
         } else {
             println!("perform_signing error");
         }
     }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
