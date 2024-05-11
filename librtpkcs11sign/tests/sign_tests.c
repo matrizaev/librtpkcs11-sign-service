5a4e3f982136242dd8e7a50ea61578d6c4af45f3
Viacheslav Matrizaev <matrizaev@gmail.com>
2024-05-11 14:23:06 +0300

    added printing slot-token info to the unit test

--- librtpkcs11sign/tests/sign_tests.c
+++ librtpkcs11sign/tests/sign_tests.c
@@ -25,6 +25,22 @@ char *test_get_slots_info()
 {
     TSlotTokenInfoArray slots = get_slots_info(PKCS11_LIBRARY_NAME);
     mu_assert(slots.count > 0 && slots.slots_info != NULL, "get_slots_info failed");
+
+    for (size_t i = 0; i < slots.count; i++)
+    {
+        if (slots.slots_info[i].valid)
+        {
+            printf("Slot %zu:\n", i);
+            printf("\tslotDescription: %s\n", slots.slots_info[i].slot_info.slotDescription);
+            printf("\tmanufacturerID: %s\n", slots.slots_info[i].slot_info.manufacturerID);
+            printf("\tmanufacturerID: %s\n", slots.slots_info[i].token_info.manufacturerID);
+            printf("\tlabel: %s\n", slots.slots_info[i].token_info.label);
+            printf("\tmodel: %s\n", slots.slots_info[i].token_info.model);
+            printf("\tserialNumber: %s\n", slots.slots_info[i].token_info.serialNumber);
+            puts("----------");
+        }
+    }
+
     return NULL;
 }
 
